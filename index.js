c918bf3bd6493ff32bb8907a070578768ae83966
Rezz88 <zzerep.88@hotmail.com>
2018-10-29 17:54:30 -0400

    Added GoogleAuth/Keys and Code refractored

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+require("./services/passport");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hey: "buddy" });
-});
+require("./routes/authRoutes")(app);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Listening - Port ${PORT}`));
